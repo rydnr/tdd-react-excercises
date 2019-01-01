1188956ae10208fb81fe2225f041a873970e207f
josesl <jose.sanleandro@osoco.es>
2019-01-01 17:49:35 +0100

    :wrench: Initial Carousel

---
+++ ch3/src/Carousel.js
@@ -0,0 +1,14 @@
+// src/Carousel.js
+import React from 'react';
+
+class Carousel extends React.PureComponent {
+  state = {
+    slideIndex: 0,
+  }
+  
+  render() {
+    return <div />;
+  }
+}
+
+export default Carousel;
